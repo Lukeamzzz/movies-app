70dae3b863d16fb7689477a9d5e00903ea7ac4fa
Luke <lukealavez@gmail.com>
2025-05-19 16:01:36 -0600

    Added trailing slash for consistency

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Configurar Next para mostrar imágenes desde una fuente externa
+  trailingSlash: true,
   images: {
     domains: ["image.tmdb.org"],
   }
